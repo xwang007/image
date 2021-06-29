f5a1bd6988788b0515c0387cf5c71082298775a4
xwang007 <>
2021-06-29 23:10:14 +0800

    update

---
+++ index.js
@@ -0,0 +1,31 @@
+const App = {
+    data() {
+        return {
+            info :""
+        }
+    },
+    methods: {
+        fileChange(e) {
+            const that = this
+            this.info = ""
+            this.$refs.imgbox.innerHTML = ""
+            const file = e.target.files[0]
+            const img = document.createElement('img')
+            img.id = 'img'
+            img.src = URL.createObjectURL(file)
+            this.$refs.imgbox.appendChild(img)
+
+            img.onload = function() {
+                EXIF.getData(document.getElementById('img'), function () {
+                    const res = JSON.stringify(EXIF.getAllTags(this),null , 4)
+                    that.info = res
+                });
+            }
+        },
+        
+    }
+
+
+}
+
+Vue.createApp(App).mount('#app')
